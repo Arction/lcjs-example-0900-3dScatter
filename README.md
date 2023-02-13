c39ca8fa990210290e15bd5897f95ff566f868ed
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:48:29 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript 3D Scatter Chart
 
-![JavaScript 3D Scatter Chart](3dScatter.png)
+![JavaScript 3D Scatter Chart](3dScatter-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,18 +8,18 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
 
-*Also known as a Scatter Graph, Scatter Series, Point Graph, Scatter diagram or Scattergram*
+_Also known as a Scatter Graph, Scatter Series, Point Graph, Scatter diagram or Scattergram_
 
 This example shows a simple 3D Point Graph with points drawn using 3D PointSeries for a visual representation of the data points 'markers'. The point graph is a type of chart or mathematical diagram drawn on a Cartesian coordinate system and represents the relationship between two variables.
 
@@ -43,9 +43,9 @@ series.add({ x: 50, y: 60, z: 40 })
 
 // Multiple points at once.
 series.add([
-    { x: 55, y: 60, z:40 },
-    { x: 60, y: 62, z:40 },
-    { x: 65, y: 65, z:50 }
+    { x: 55, y: 60, z: 40 },
+    { x: 60, y: 62, z: 40 },
+    { x: 65, y: 65, z: 50 },
 ])
 ```
 
@@ -60,23 +60,23 @@ series.add([
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[3D chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chart3d.html
-[3D point series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/pointseries3d.html
+[3D chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Chart3D.html
+[3D point series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/PointSeries3D.html
 
--- src/index.js
+++ src/index.js
@@ -4,25 +4,20 @@
 // Import LightningChartJS
 const lcjs = require('@arction/lcjs')
 
+// Import xydata
+const xydata = require('@arction/xydata')
+
 // Extract required parts from LightningChartJS.
-const {
-    lightningChart,
-    SolidFill,
-    ColorRGBA,
-    PointStyle3D,
-    Themes
-} = lcjs
+const { lightningChart, SolidFill, ColorRGBA, PointStyle3D, Themes } = lcjs
 
 // Extract required parts from xyData.
-const {
-    createWaterDropDataGenerator
-} = require('@arction/xydata')
+const { createWaterDropDataGenerator } = xydata
 
 // Initiate chart
-const chart3D = lightningChart().Chart3D({
-    disableAnimations: true,
-    // theme: Themes.darkGold
-})
+const chart3D = lightningChart()
+    .Chart3D({
+        // theme: Themes.darkGold
+    })
     .setTitle('3D Scatter Chart')
 
 // Set Axis titles
@@ -31,29 +26,32 @@ chart3D.getDefaultAxisY().setTitle('Axis Y')
 chart3D.getDefaultAxisZ().setTitle('Axis Z')
 
 // Create Point Series for rendering max Y coords.
-const pointSeriesMaxCoords = chart3D.addPointSeries()
-    .setPointStyle(new PointStyle3D.Triangulated({
-        fillStyle: new SolidFill({ color: ColorRGBA(224, 152, 0) }),
+const pointSeriesMaxCoords = chart3D.addPointSeries().setName('Max coords')
+pointSeriesMaxCoords.setPointStyle(
+    new PointStyle3D.Triangulated({
+        fillStyle: pointSeriesMaxCoords.getPointStyle().getFillStyle(),
         size: 10,
-        shape: 'sphere'
-    }))
-    .setName('Max coords')
+        shape: 'sphere',
+    }),
+)
 
 // Create another Point Series for rendering other Y coords than Max.
-const pointSeriesOtherCoords = chart3D.addPointSeries()
-    .setPointStyle(new PointStyle3D.Triangulated({
-        fillStyle: new SolidFill({ color: ColorRGBA(255, 0, 0) }),
+const pointSeriesOtherCoords = chart3D.addPointSeries({ automaticColorIndex: 2 }).setName('Below Max')
+pointSeriesOtherCoords.setPointStyle(
+    new PointStyle3D.Triangulated({
+        fillStyle: pointSeriesOtherCoords.getPointStyle().getFillStyle(),
         size: 5,
-        shape: 'cube'
-    }))
-    .setName('Below Max')
+        shape: 'cube',
+    }),
+)
 
 // Add LegendBox to chart.
-chart3D.addLegendBox()
+chart3D
+    .addLegendBox()
     // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
     .setAutoDispose({
         type: 'max-width',
-        maxWidth: 0.30,
+        maxWidth: 0.3,
     })
     .add(chart3D)
 
@@ -62,31 +60,30 @@ let totalPointsAmount = 0
 const rows = 40
 const columns = 60
 createWaterDropDataGenerator()
-    .setRows( rows )
-    .setColumns( columns )
+    .setRows(rows)
+    .setColumns(columns)
     .generate()
-    .then( data => {
+    .then((data) => {
         // 'data' is a number Matrix number[][], that can be read as data[row][column].
-        for ( let row = 0; row < rows; row ++ ) {
-            for ( let column = 0; column < columns; column ++ ) {
+        for (let row = 0; row < rows; row++) {
+            for (let column = 0; column < columns; column++) {
                 const value = data[row][column]
                 // Generate 'value' amount of points along this XZ coordinate,
                 // with the Y coordinate range based on 'value'.
-                const pointsAmount = Math.ceil( value / 100 )
+                const pointsAmount = Math.ceil(value / 100)
                 const yMin = 0
                 const yMax = value
-                for ( let iPoint = 0; iPoint < pointsAmount; iPoint ++ ) {
+                for (let iPoint = 0; iPoint < pointsAmount; iPoint++) {
                     const y = yMin + Math.random() * (yMax - yMin)
                     pointSeriesOtherCoords.add({ x: row, z: column, y })
-                    totalPointsAmount ++
+                    totalPointsAmount++
                 }
                 pointSeriesMaxCoords.add({ x: row, z: column, y: yMax })
-                totalPointsAmount ++
+                totalPointsAmount++
             }
         }
 
         chart3D.setTitle(chart3D.getTitle() + ` (${totalPointsAmount} data points)`)
         // Set explicit Y Axis interval.
-        chart3D.getDefaultAxisY().setInterval(0, 150, 2000, true)
+        chart3D.getDefaultAxisY().setInterval({ start: 0, end: 150, animate: 2000 })
     })
-
